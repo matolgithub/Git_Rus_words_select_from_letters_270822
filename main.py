36c9d9065ac58f910c11ff80af50e0b334a189ed
matolgithub <89175955650@mail.ru>
2022-08-27 18:25:00 +0300

    Final commit.

--- main.py
+++ main.py
@@ -9,13 +9,31 @@ def decoder():
         ru.write(text.encode('utf-8'))
 
 
-def search_words(list_letters=["а", "с", "п", "у", "р"]):
+def search_words(list_letters=["а", "в", "т", "а"]):
     words_number = 0
     possible_words_list = []
     search_words_list = []
+
     for item in list(itertools.permutations(list_letters)):
         possible_words_list.append(''.join(item))
+        possible_words_list = list(set(possible_words_list))
+
     print(possible_words_list)
+
+    for word in possible_words_list:
+        print(f'//////    word:  {word}    searching  ///////')
+        with open('russian_decoding.txt', 'r', encoding='utf-8') as file:
+            rus_words = file.readlines()
+            if word in [item.strip() for item in rus_words]:
+                words_number += 1
+                print(f'---- Word {word} - success! Number: {words_number} ---------')
+                if word not in search_words_list:
+                    search_words_list.append(word)
+
+    if search_words_list == []:
+        print('The result of the search is negative! There is no one word!')
+    else:
+        print(f'The result of the search is positive! The list of searching words is:\n{search_words_list}.')
 
 
 if __name__ == '__main__':
